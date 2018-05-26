a299d45a7b1a8bf01678981df670938562b01830
realChainLife <realchainlife@outlook.com>
2018-05-26 16:07:50 +0300

    update function descriptions

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -1,33 +1,13 @@
 pragma solidity ^0.4.20;
 
 contract Token {
-    /* This is a slight change to the ERC20 base standard.
-    function totalSupply() constant returns (uint256 supply);
-    is replaced with:
-    uint256 public totalSupply;
-    This automatically creates a getter function for the totalSupply.
-    This is moved to the base contract since public getter functions are not
-    currently recognised as an implementation of the matching abstract
-    function by the compiler.
-    */
-    /// total amount of tokens
+   
     uint256 public totalSupply;
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
     function balanceOf(address _owner) view public returns (uint256 balance);
 
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) public returns (bool success);
 
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
 
     /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
