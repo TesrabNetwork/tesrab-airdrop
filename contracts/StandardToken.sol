42f8c28fbace2ef467bd70726a38065d36640348
realChainLife <realchainlife@outlook.com>
2018-05-26 17:39:02 +0300

    update function descriptions

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -31,8 +31,7 @@ contract StandardToken is Token {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]);
+        
         require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
         balances[_to] += _value;
         balances[_from] -= _value;
